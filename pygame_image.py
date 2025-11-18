0271e673abdab7aba60446372fb15b76123cec6b
FELLIN <c0a242657d@edu.teu.ac.jp>
2025-11-18 16:50:20 +0900

    問題８完了

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flipped = pg.transform.flip(bg_img, True, False)#問題８
     kokaton_img = pg.image.load("fig/3.png")#問題３
     kokaton_img = pg.transform.flip(kokaton_img, True, False)#問題３
     tmr = 0
@@ -18,7 +19,7 @@ def main():
             if event.type == pg.QUIT: return
         x = tmr # 練習５
         screen.blit(bg_img, [-x, 0]) # 練習５
-        screen.blit(bg_img, [-x+1600, 0]) # 練習７
+        screen.blit(bg_img_flipped, [-x+1600, 0]) # 練習７
         screen.blit(kokaton_img,[300,200])#問題４
         pg.display.update()
         tmr += 1        
