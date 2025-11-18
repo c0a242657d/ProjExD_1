2db0bfe1c3a5f7ffa26918c497c988a2d3e56adf
FELLIN <c0a242657d@edu.teu.ac.jp>
2025-11-18 17:06:27 +0900

    問題１０完了

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,32 @@ def main():
     bg_img_flipped = pg.transform.flip(bg_img, True, False)#問題８
     kokaton_img = pg.image.load("fig/3.png")#問題３
     kokaton_img = pg.transform.flip(kokaton_img, True, False)#問題３
+    koukaton_rect = kokaton_img.get_rect()#問題１０
+    koukaton_rect.center = 300, 200#問題１０
+   
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr # 練習５
+        
+        x = tmr % 3200 # 練習５・９
         screen.blit(bg_img, [-x, 0]) # 練習５
         screen.blit(bg_img_flipped, [-x+1600, 0]) # 練習７
-        screen.blit(kokaton_img,[300,200])#問題４
+        screen.blit(bg_img, (-x + 3200, 0))#問題８
+        screen.blit(kokaton_img,koukaton_rect)#問題４
         pg.display.update()
         tmr += 1        
         clock.tick(200)
+
+        key_list = pg.key.get_pressed()#問題１０
+        if key_list[pg.K_UP]:#問題１０
+            koukaton_rect.move_ip(0, -1)
+        if key_list[pg.K_DOWN]:#問題１０
+            koukaton_rect.move_ip(0, +1)
+        if key_list[pg.K_LEFT]:#問題１０
+            koukaton_rect.move_ip(-1, 0)
+        if key_list[pg.K_RIGHT]:#問題１０
+            koukaton_rect.move_ip(+1, 0)
 
 
 if __name__ == "__main__":
